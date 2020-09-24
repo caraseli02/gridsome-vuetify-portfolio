92a3c708361729756c445cf6eff1cf790129a19c
vlad <caraseli02@gmail.com>
2020-09-24 22:01:57 +0200

    add purgecss

--- gridsome.config.js
+++ gridsome.config.js
@@ -33,6 +33,20 @@ module.exports = {
                     'en-gb': require('./src/locales/en-gb.json'),
                 }
             }
+        },
+        {
+            use: 'gridsome-plugin-purgecss',
+            // default options, the following will be included if you don't provide anything
+            options: {
+                content: [
+                    './src/**/*.vue',
+                    './src/**/*.js',
+                    './src/**/*.jsx',
+                    './src/**/*.pug',
+                    './src/**/*.md'
+                ],
+                defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+            }
         }
     ],
     chainWebpack: config => {
